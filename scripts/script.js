cc0513c57ef8bac0ab8c8f69107abd1628bc4d97
Matthijs217 <matthijstenbrink@gmail.com>
2025-01-14 10:22:34 +0100

    **

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+const charactersButtons = document.querySelectorAll('.character-section ul button');
+const teamList = document.querySelector('.team-section ul');
+const progress = document.querySelector('progress');
+const teamCounter = document.querySelector('#team-counter span')
+
+charactersButtons.forEach( charactersButton => {
+    charactersButton.addEventListener('click', addCharacterToTeam);
+});
+
+function addCharacterToTeam(event) {
+    const characterButton = event.currentTarget;
+
+    const charactersImage = characterButton.querySelector("img");
+    const characterName = charactersImage.alt;
+    const charactersImageClone = charactersImage.cloneNode();
+
+    const deleteButton = document.createElement('button');
+    deleteButton.ariaLabel = `Verwijder ${characterName}`;
+    deleteButton.addEventListener('click', removeCharacterFromTeam);
+
+    const firstEmptySlot =  teamList.querySelector('li:empty');
+
+
+    firstEmptySlot.appendChild(charactersImageClone);
+    firstEmptySlot.appendChild(deleteButton);
+
+    updateCounterAndProgress(+1);
+}
+
+function removeCharacterFromTeam(event) {
+    const deleteButton = event.currentTarget;
+    const slot = deleteButton.closest('li');
+    const characterImg = slot.querySelector('img');
+
+    deleteButton.remove();
+    characterImg.remove();
+
+    updateCounterAndProgress(-1);
+}
+
+function updateCounterAndProgress( delta ) {
+    progress.value = progress.value + delta;
+
+
+    const currentCount = teamCounter.textContent;
+    const newCount = currentCount - delta;
+    teamCounter.textContent = newCount;
+}
+
